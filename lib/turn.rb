e02539625fa7830eae1666e42805ac12ce8e8e4d
Ryan Davey <ryancdavey@icloud.com>
2018-06-27 23:16:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,10 +18,9 @@ def valid_move?(board,index)
   end
 end
 
-def input_to_index(user_input)
-  index = user_input.to_i - 1
-  return index
-end
+def input_to_index(input)
+  input.to_i - 1
+end 
 
 def position_taken?(board, index)
     if board[index] == " " || board[index] == "" || board[index] == nil
@@ -32,19 +31,19 @@ def position_taken?(board, index)
 end
 
 def move(board, index, token = "X")
-  board[index] = " #{token} "
-  display_board(board)
+  board[index] = "#{token}"
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
-  input_index = input_to_index(gets.chomp)
   
-  if valid_move?(board,input_index)
-    puts "Please enter a token:"
-    user_token = gets.chomp
-    move(board,input_index,user_token)
-  else 
-    turn(board)
-  end
+  puts "Please enter 1-9:"
+    user_index = gets.chomp
+    input_index = input_to_index(user_index)
+    if valid_move?(board, input_index)
+      move(board, input_index)
+      puts display_board(board)
+    else
+      puts "That is an invalid entry!"
+      turn(board)
+    end 
 end
